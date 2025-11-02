868814172c169153497098bce42c631c20d49cd9
truestbyheart <margalight@outlook.com>
2025-11-02 22:14:15 +0200

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./partials/**/*.hbs",
+    "./default.hbs",
+    "./index.hbs",
+    "./post.hbs",
+    "./page.hbs",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
